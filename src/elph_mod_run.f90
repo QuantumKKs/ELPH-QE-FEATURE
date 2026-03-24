8bccd72c849c05e6e9c37db793010caecb6a599e
sanjaygrr <65647041+sanjaygrr@users.noreply.github.com>
2026-03-23 21:19:30 -0300

    Actualizar autoría en los archivos de código fuente

--- src/elph_mod_run.f90
+++ src/elph_mod_run.f90
@@ -1,5 +1,5 @@
 !
-! Copyright (C) 2024 Quantum ESPRESSO group
+! Authors: Sanjay Gopal Ramchandani, Fabian Jofré Parra
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
